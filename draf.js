39c71442dd450a924868e147d7bc08d4fa3f09e1
Danilo Couto <couto.danilo95@gmail.com>
2021-10-25 10:56:02 -0300

    requisito 5

--- draf.js
+++
@@ -1,9 +0,0 @@
-/* var htmlExemplo = 'SKU: MLB1937267358 | NAME: Cpu Pc Gamer Intel I5 24008gb Ssd 240gb Geforce Gt610 2gb | PRICE: $1899';
-var resultado = htmlExemplo.substring(htmlExemplo.length-5);
-
-console.log(resultado); */
-
-var htmlExemplo = 'SKU: MLB1937267358 | NAME: Cpu Pc Gamer Intel I5 24008gb Ssd 240gb Geforce Gt610 2gb | PRICE: $1899';
-var resultado = htmlExemplo.split('$').pop();
-
-console.log(resultado);
